d032987b2eb14fb82672b9cc05ba20fc97db386f
thomasismartinez <tjm7126@rit.edu>
2022-08-24 12:57:45 -0400

    PE1 HelloWorld

---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColorAndNumber
+{
+    namespace Color
+    {
+        static public class ColorClass
+        {
+            public static string sFavColor;
+        }
+    }
+    namespace Number
+    {
+        static public class NumberClass
+        {
+            public static string nFavNumber;
+        }
+    }
+
+    using CAlias = FavoriteColorAndNumber.Color
+
+    internal class Program
+    {
+        /*
+         * Method: Main
+         * Purpose: Prompt the user for their favorite color and number
+         *      Output their their favorite color (in limited text colors) their favorite number of times
+         * Restrictions: None
+         */
+        static void Main(string[] args)
+        {
+            string sFavColor = null;
+            sFavColor = "";
+            sFavColor = sFavColor.ToUpper();
+
+            int? nFavNumber = null;
+
+            //prompt user for their favorite color
+            System.Console.Write("Enter your favorite color:\t");
+
+            //get the user's input and store it in a variable
+            //Console.ReadLine();
+
+            Color.ColorClass.sFavColor = System.Console.ReadLine();
+
+            //promp the user for their favorite number
+            System.Console.Write("Enter your favorite number:\t");
+
+            do
+            {
+                try
+                {
+                    //get the user's input and store it in a variable
+                    Number.NumberClass.nFavNumber = Convert.ToInt32(System.Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an interger");
+                }
+            } while (nFavNumber == null);
+            //validate that the user entered a valid number
+
+            //change the output text color to match their favorite color
+            switch(sFavColor.ToLower()
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    break;
+            }
+
+            //output their favorite color the number of times as their favorite number
+            for (int i = 0; i < Number.NumberClass.nFavNumber; i++)
+            {
+                //System.Console.WriteLine("Your favorite color is " + sFavColor + "!");
+                System.Console.WriteLine($"Your favorite color is {Color.ColorClass.sFavColor}!");
+            }
+        }
+    }
+}
---
+++ Martinez_HelloWorld/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Martinez_HelloWorld
+{
+    /*
+    Class: Program
+    Author: Thomas Martinez
+    Purpose: Introduction to C# programming
+    Restrictions: This is my first time programming C#
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            Console.WriteLine("Hello World!");
+            int number = 3;
+            Console.WriteLine("int number has been set to 3");
+            int digit = 4;
+            Console.WriteLine("int digit has been set to 4");
+            double dec = 3.0;
+            Console.WriteLine("double decimal has been set to 3.0");
+            digit = (int) dec;
+            Console.WriteLine("int digit has been set the the casted value of dec");
+            dec = 4;
+            Console.WriteLine("double dec has been set to 4.0 (via implicit casting)");
+
+            //while loop counts down number until it hits 0
+            while (number > 0)
+            {
+                Console.WriteLine("number = " + number);
+                //if else tells user weather the number is EVEN or ODD
+                if ((number % 2) == 0)
+                {
+                    Console.WriteLine("NUMBER IS EVEN");
+                }
+                else
+                {
+                    Console.WriteLine("NUMBER IS ODD");
+                }
+                number--;
+            }
+
+            //user input prompt
+            Console.WriteLine("Please input 6 words");
+            //for loop takes word input 6 times, each time adding the new word/line to an ArrayList
+            ArrayList words = new ArrayList();
+            for (int i = 0; i < 6 ; i++)
+            {
+                words.Add(Console.ReadLine());
+            }
+            //iterates through ArrayList to display the users input in a comma'd list
+            Console.Write("Thank you for writing: ");
+            foreach (String word in words)
+            {
+                Console.Write(word + ", ");
+            }
+        }
+    }
+}
