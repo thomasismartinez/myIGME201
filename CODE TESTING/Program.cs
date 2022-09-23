b9e8b03791c5c644d8bb353cf5cd38319efbe430
thomasismartinez <tjm7126@rit.edu>
2022-09-22 22:25:57 -0400

    q1

---
+++ CODE TESTING/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE_TESTING
+{
+    internal class Program
+    {
+        struct order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+            public double totalCost()
+            {
+                return unitCost * unitCount;
+            }
+            public FormattableString orderPrint()
+            {
+                FormattableString orderString =
+                    $"{unitCount} {itemName} items at ${unitCost} each, total cost ${totalCost()}";
+                return orderString;
+            }
+        }
+        static void Main(string[] args)
+        {
+            order O = new order();
+            O.itemName = "Computer";
+            O.unitCost = 20;
+            O.unitCount = 5;
+
+            Console.WriteLine(O.orderPrint());
+        }
+    }
+}
---
+++ UNIT_TEST_1-Q1/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIT_TEST_1_Q1
+{
+    /// <summary>
+    /// Class: Program
+    /// Author: David Schuh and Thomas Martinez
+    /// Purpose: Unit Test 1 Question 1
+    /// Restrictions: None
+    /// </summary>
+    static internal class Program
+    {
+        /// <summary>
+        /// Method: Main
+        /// Purpose: Math quiz with Division
+        /// Restrictions: None
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // store user name
+            string myName = "";
+
+            // string and int of # of questions
+            string sQuestions = "";
+            int nQuestions = 0;
+
+            // string and base value related to difficulty
+            string sDifficulty = "";
+            int nMaxRange = 0;
+
+            // constant for setting difficulty with 1 variable
+            const int MAX_BASE = 10;
+
+            // question and # correct counters
+            int nCntr = 0;
+            int nCorrect = 0;
+
+            // operator picker
+            int nOp = 0;
+
+            // operands and solution
+            double val1 = 0.00;
+            double val2 = 0.00;
+            double nAnswer = 0.00;
+
+            // string and int for the response
+            string sResponse = "";
+            double nResponse = 0;
+
+            // boolean for checking valid input
+            bool bValid = false;
+
+            // play again?
+            string sAgain = "";
+
+            // seed the random number generator
+            Random rand = new Random();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Math Quiz!");
+            Console.WriteLine();
+
+            // fetch the user's name into myName
+            while (true)
+            {
+                Console.Write("What is your name-> ");
+                myName = Console.ReadLine();
+
+                if (myName.Length > 0)
+                {
+                    break;
+                }
+            }
+
+        // label to return to if they want to play again
+        start:
+
+            // initialize correct responses for each time around
+            nCorrect = 0;
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("How many questions-> ");
+                sQuestions = Console.ReadLine();
+
+                try
+                {
+                    nQuestions = int.Parse(sQuestions);
+                    bValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer.");
+                    bValid = false;
+                }
+
+            } while (!bValid);
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Difficulty level (easy, medium, hard)-> ");
+                sDifficulty = Console.ReadLine();
+            } while (sDifficulty.ToLower() != "easy" &&
+                     sDifficulty.ToLower() != "medium" &&
+                     sDifficulty.ToLower() != "hard");
+
+            Console.WriteLine();
+
+            // if they choose easy, then set nMaxRange = MAX_BASE, unless myName == "David", then set difficulty to hard
+            // if they choose medium, set nMaxRange = MAX_BASE * 2
+            // if they choose hard, set nMaxRange = MAX_BASE * 3
+            switch (sDifficulty.ToLower())
+            {
+                case "easy":
+                    nMaxRange = MAX_BASE;
+                    if (myName.ToLower() == "david")
+                    {
+                        goto case "hard";
+                    }
+                    break;
+
+                case "medium":
+                    nMaxRange = MAX_BASE * 2;
+                    break;
+
+                case "hard":
+                    nMaxRange = MAX_BASE * 3;
+                    break;
+            }
+
+            //Inform user of input format
+            Console.WriteLine("Please enter integers values or double values rounded to now more than 2 places. :)\n");
+
+            // ask each question
+            for (nCntr = 0; nCntr < nQuestions; ++nCntr)
+            {
+                // generate a random number between 0 inclusive and 3 exclusive to get the operation
+                nOp = rand.Next(0, 4);
+
+                val1 = rand.Next(0, nMaxRange) + nMaxRange;
+                val2 = rand.Next(0, nMaxRange);
+
+                // if either argument is 0, pick new numbers
+                if (val1 == 0 || val2 == 0)
+                {
+                    // decrement counter to try this one again (because it will be incremented at the top of the loop)
+                    --nCntr;
+                    continue;
+                }
+
+                // if nOp == 0, then addition
+                // if nOp == 1, then subtraction
+                // else multiplication
+                if (nOp == 0)
+                {
+                    nAnswer = val1 + val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} + {val2} => ";
+                }
+                else if (nOp == 1)
+                {
+                    nAnswer = val1 - val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} - {val2} => ";
+                }
+                else if (nOp == 2)
+                {
+                    nAnswer = val1 * val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} * {val2} => ";
+                }
+                else
+                {
+                    nAnswer = Math.Round( (val1 / val2), 2 );
+                    sQuestions = $"Question #{nCntr + 1}: {val1} / {val2} =>";
+                }
+
+                // display the question and prompt for the answer
+                do
+                {
+                    Console.Write(sQuestions);
+                    sResponse = Console.ReadLine();
+
+                    try
+                    {
+                        nResponse = double.Parse(sResponse);
+                        bValid = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a double rounded to 2 decimal places.");
+                        bValid = false;
+                    }
+
+                } while (!bValid);
+
+                // if response == answer, output flashy reward and increment # correct
+                // else output stark answer
+                if (nResponse == nAnswer)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Well done, {0}!!!", myName);
+
+                    ++nCorrect;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("I'm sorry {0}. The answer is {1}", myName, nAnswer);
+                }
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            // output how many they got correct and their score
+            Console.WriteLine("You got {0} correct out of {1}, which is a score of {2:P2}", nCorrect, nQuestions, Convert.ToDouble(nCorrect) / (double)nCntr);
+            Console.WriteLine();
+
+            do
+            {
+                // prompt if they want to play again
+                Console.Write("Do you want to play again? ");
+
+                sAgain = Console.ReadLine();
+
+                if (sAgain.ToLower().StartsWith("y"))
+                {
+                    goto start;
+                }
+                else if (sAgain.ToLower().StartsWith("n"))
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
