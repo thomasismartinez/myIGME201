8119a34476f83c97c4a43951089ed0764440c16f
thomasismartinez <tjm7126@rit.edu>
2022-09-30 17:36:30 -0400

    g

--- Traffic/Program.cs
+++ Traffic/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vehicles;
 
 namespace Traffic
 {
     static internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            // create an object of a class that inherits IPassengerCarrier
+            Compact myCompact = new Compact();
+
+            // create an object of a class that does not inherit IPassengerCarrier
+            FreightTrain myFreightTrain = new FreightTrain();
+
+            // call AddPassenger with an IPassengerCarrier object
+            AddPassenger(myCompact);
+
+            // call AddPassenger with a non-IPassengerCarrier object
+            //AddPassenger(myFreightTrain);
+            // causes a compile-time error because FreightTrain does not implement
+            // the IPassengerCarrier interface so it won't pass as a parameter
+        }
+
+        public static void AddPassenger(IPassengerCarrier vehicle)
+        {
+            vehicle.LoadPassenger();
+            Console.WriteLine(vehicle.ToString());
         }
     }
 }
