bf9da843a611ff2fa247df260c87b46c800f1645
thomasismartinez <tjm7126@rit.edu>
2022-10-03 17:06:10 -0400

    deleted stuff

--- CODE TESTING/Program.cs
+++ CODE TESTING/Program.cs
@@ -9,30 +9,9 @@ namespace CODE_TESTING
 {
     internal class Program
     {
-        struct order
-        {
-            public string itemName;
-            public int unitCount;
-            public double unitCost;
-            public double totalCost()
-            {
-                return unitCost * unitCount;
-            }
-            public FormattableString orderPrint()
-            {
-                FormattableString orderString =
-                    $"{unitCount} {itemName} items at ${unitCost} each, total cost ${totalCost()}";
-                return orderString;
-            }
-        }
         static void Main(string[] args)
         {
-            order O = new order();
-            O.itemName = "Computer";
-            O.unitCost = 20;
-            O.unitCount = 5;
 
-            Console.WriteLine(O.orderPrint());
         }
     }
 }
