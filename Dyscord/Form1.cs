bd98db253ef3d45fd490e4a0af16a8e59f6002cd
thomasismartinez <tjm7126@rit.edu>
2022-11-20 21:45:19 -0500

    2

--- Dyscord/Form1.cs
+++ Dyscord/Form1.cs
@@ -12,9 +12,37 @@ namespace Dyscord
 {
     public partial class SettingsForm : Form
     {
-        public SettingsForm()
+        public int myPort;
+        public SettingsForm(Form owner, int nPort)
         {
             InitializeComponent();
+
+            this.Owner = owner;
+            this.CenterToParent();
+
+            this.myPort = nPort;
+            this.portTextBox.Text = nPort.ToString();
+
+            this.startButton.Click += new EventHandler(StartButton__Click);
+            this.portTextBox.KeyPress += new KeyPressEventHandler(PortTextBox__KeyPress);
+        }
+
+        private void StartButton__Click(object sender, EventArgs e)
+        {
+            this.myPort = Int32.Parse(this.portTextBox.Text);
+            this.Close();
+        }
+
+        private void PortTextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(Char.IsDigit(e.KeyChar) || e.KeyChar == '\b')
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
         }
     }
 }
