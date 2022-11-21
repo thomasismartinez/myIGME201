9a189cba47abdba45edbe677627f35042f4766e9
thomasismartinez <tjm7126@rit.edu>
2022-11-20 23:09:52 -0500

    4

--- Dyscord/Form2.cs
+++ Dyscord/Form2.cs
@@ -11,6 +11,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Xml;
 
 namespace Dyscord
 {
@@ -18,10 +20,10 @@ namespace Dyscord
 
     public partial class DyscordForm : Form
     {
-    string targetUser; // the username to communicate with
- 	string targetIp; // the ip address of the user to communicate with
+    string targetUser = ""; // the username to communicate with
+ 	string targetIp = ""; // the ip address of the user to communicate with
     int targetPort; // the port of the user to communicate with
-    string myIp; // your ip address
+    string myIp = ""; // your ip address
     int myPort = 2222; // my listener port
     Thread thread;
  	Socket listener;
@@ -38,6 +40,103 @@ namespace Dyscord
             ThreadStart threadStart = new ThreadStart(Listen);
             thread = new Thread(threadStart);
             thread.Start();
+
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(IPAddress iPAddress in ipHost.AddressList)
+            {
+                if (iPAddress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    this.myIp = iPAddress.ToString();
+                    break;
+                }
+            }
+
+            this.loginButton.Click += new EventHandler(LoginButton__Click);
+            this.usersButton.Click += new EventHandler(UsersButton__Click);
+            this.sendButton.Click += new EventHandler(SendButton__Click);
+            this.exitButton.Click += new EventHandler(ExitButton__Click);
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser1__DocumentCompleted);
+        }
+
+        private void LoginButton__Click(object sender, EventArgs e)
+        {
+            if (userTextBox.TextLength > 0)
+            {
+                webBrowser1.Navigate("http://people.rit.edu/dxsigm/php/login.php?login=" + 
+                    userTextBox.Text + "&ip=" + myIp + ":" + myPort);
+            }
+            webBrowser1.Visible = false;
+            userTextBox.Enabled = false;
+            loginButton.Enabled = false;
+        }
+
+        private void UsersButton__Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://people.rit.edu/dxsigm/php/login.php?login=");
+            webBrowser1.Visible = true;
+            convRichTextBox.SendToBack();
+        }
+
+        private void SendButton__Click(object sender, EventArgs e)
+        {
+            if (this.targetIp.Length > 0)
+            {
+                IPAddress iPAddress = IPAddress.Parse(this.targetIp);
+                IPEndPoint remoteEndPoint = new IPEndPoint(iPAddress, this.targetPort);
+
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                server.Connect(remoteEndPoint);
+                Stream netStream = new NetworkStream(server);
+                StreamWriter writer = new StreamWriter(netStream);
+
+                string msg = userTextBox.Text + ": " + messageRichTextBox.Text;
+                writer.WriteLine(msg.ToCharArray(), 0, msg.Length);
+
+                writer.Close();
+                netStream.Close();
+                server.Close();
+
+                this.convRichTextBox.Text += "> " + this.targetUser + ": " + messageRichTextBox.Text + "\n";
+
+                this.messageRichTextBox.Clear();
+            }
+        }
+
+        private void ExitButton__Click(object sender, EventArgs e)
+        {
+            listener.Close();
+            thread.Abort();
+
+            Application.Exit();
+        }
+
+        private void WebBrowser1__DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlElementCollection htmlElementCollection;
+            htmlElementCollection = webBrowser1.Document.GetElementsByTagName("button");
+            foreach (HtmlElement htmlElement in htmlElementCollection)
+            {
+                htmlElement.Click += new HtmlElementEventHandler(HtmlElement__Click);
+            }
+        }
+
+        private void HtmlElement__Click(object sender, HtmlElementEventArgs e)
+        {
+            string title;
+            string[] ipPort;
+
+            HtmlElement htmlElement = (HtmlElement)sender;
+
+            title = htmlElement.GetAttribute("title");
+            ipPort = title.Split(':');
+            this.targetIp = ipPort[0];
+            this.targetPort = Int32.Parse(ipPort[1]);
+
+            this.targetUser = htmlElement.GetAttribute("name");
+            this.groupBox1.Text = "Conversing with " + targetUser;
+
+            webBrowser1.Visible = false;
+            webBrowser1.SendToBack();
         }
 
         private void UpdateConversation(string text)
