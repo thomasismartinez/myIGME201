d5eaaae9e4304e61549de97f6e12fc0032fde85e
thomasismartinez <tjm7126@rit.edu>
2022-11-22 16:28:56 -0500

    1

---
+++ JsonHtml/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using PeopleAppGlobals;
+using PeopleLib;
+
+namespace JsonHtml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Globals.AddCoursesSampleData();
+
+            List<Teacher> teachers = new List<Teacher>();
+            List<Student> students = new List<Student>();
+
+            foreach (KeyValuePair<string,Person> keyValuePair in Globals.people.sortedList)
+            {
+                if(keyValuePair.Value.GetType() == typeof(Teacher))
+                {
+                    teachers.Add((Teacher)keyValuePair.Value);
+                }
+                else
+                {
+                    students.Add((Student)keyValuePair.Value);
+                }
+            }
+
+            string s = JsonConvert.SerializeObject(students);
+        }
+    }
+}
