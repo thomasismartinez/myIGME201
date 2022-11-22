37ae9423a3faa0606bfa09d7292f86d01f488126
thomasismartinez <tjm7126@rit.edu>
2022-11-22 18:14:29 -0500

    1

---
+++ TrivaApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrivaApp
+{
+    class TriviaResult
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public List<string> incorrect_answers;
+    }
+    class Triva
+    {
+        public int responce_code;
+        public List<TriviaResult> triviaResults;
+
+    }
+    /*
+     * {"response_code":0,
+     * "results":[{"category":"Entertainment: Video Games","type":"multiple","difficulty":"medium"
+     * ,"question":"What was the first interactive movie video game?",
+     * "correct_answer":"Astron Belt","incorrect_answers":["Dragon&#039;s Lair","Cube Quest","M.A.C.H. 3"]}]}
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
