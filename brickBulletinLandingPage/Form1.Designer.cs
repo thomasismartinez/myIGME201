d17b5ab83f5f643403b6ad884d270871bc89130f
thomasismartinez <tjm7126@rit.edu>
2022-12-07 23:46:37 -0500

    first wait no

--- brickBulletinLandingPage/Form1.Designer.cs
+++ brickBulletinLandingPage/Form1.Designer.cs
@@ -29,29 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.searchButton = new System.Windows.Forms.Button();
             this.createPostButton = new System.Windows.Forms.Button();
             this.refreshButton = new System.Windows.Forms.Button();
             this.settingsButton = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.feedFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.filterFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.loginButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // logoPictureBox
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(148, 78);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.logoPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(148, 78);
+            this.logoPictureBox.TabIndex = 1;
+            this.logoPictureBox.TabStop = false;
             // 
             // searchButton
             // 
-            this.searchButton.Location = new System.Drawing.Point(237, 36);
+            this.searchButton.Location = new System.Drawing.Point(575, 22);
             this.searchButton.Name = "searchButton";
             this.searchButton.Size = new System.Drawing.Size(75, 23);
             this.searchButton.TabIndex = 2;
@@ -60,16 +61,17 @@
             // 
             // createPostButton
             // 
-            this.createPostButton.Location = new System.Drawing.Point(349, 35);
+            this.createPostButton.Location = new System.Drawing.Point(204, 51);
             this.createPostButton.Name = "createPostButton";
             this.createPostButton.Size = new System.Drawing.Size(103, 23);
             this.createPostButton.TabIndex = 3;
             this.createPostButton.Text = "Create Post";
             this.createPostButton.UseVisualStyleBackColor = true;
+            this.createPostButton.Visible = false;
             // 
             // refreshButton
             // 
-            this.refreshButton.Location = new System.Drawing.Point(488, 35);
+            this.refreshButton.Location = new System.Drawing.Point(941, 82);
             this.refreshButton.Name = "refreshButton";
             this.refreshButton.Size = new System.Drawing.Size(75, 23);
             this.refreshButton.TabIndex = 4;
@@ -78,58 +80,75 @@
             // 
             // settingsButton
             // 
-            this.settingsButton.Location = new System.Drawing.Point(610, 35);
+            this.settingsButton.Location = new System.Drawing.Point(941, 22);
             this.settingsButton.Name = "settingsButton";
             this.settingsButton.Size = new System.Drawing.Size(75, 23);
             this.settingsButton.TabIndex = 5;
             this.settingsButton.Text = "Settings";
             this.settingsButton.UseVisualStyleBackColor = true;
             // 
-            // flowLayoutPanel1
+            // feedFlowLayoutPanel
             // 
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(166, 111);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(865, 446);
-            this.flowLayoutPanel1.TabIndex = 6;
+            this.feedFlowLayoutPanel.AutoScroll = true;
+            this.feedFlowLayoutPanel.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.feedFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.feedFlowLayoutPanel.Location = new System.Drawing.Point(166, 111);
+            this.feedFlowLayoutPanel.Name = "feedFlowLayoutPanel";
+            this.feedFlowLayoutPanel.Size = new System.Drawing.Size(850, 450);
+            this.feedFlowLayoutPanel.TabIndex = 6;
+            this.feedFlowLayoutPanel.WrapContents = false;
             // 
-            // flowLayoutPanel2
+            // filterFlowLayoutPanel
             // 
-            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 111);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(148, 446);
-            this.flowLayoutPanel2.TabIndex = 7;
+            this.filterFlowLayoutPanel.AutoScroll = true;
+            this.filterFlowLayoutPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.filterFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.filterFlowLayoutPanel.Location = new System.Drawing.Point(12, 111);
+            this.filterFlowLayoutPanel.Name = "filterFlowLayoutPanel";
+            this.filterFlowLayoutPanel.Size = new System.Drawing.Size(150, 450);
+            this.filterFlowLayoutPanel.TabIndex = 7;
+            this.filterFlowLayoutPanel.WrapContents = false;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(204, 22);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 9;
+            this.loginButton.Text = "Log In";
+            this.loginButton.UseVisualStyleBackColor = true;
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 569);
-            this.Controls.Add(this.flowLayoutPanel2);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(1024, 569);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.filterFlowLayoutPanel);
+            this.Controls.Add(this.feedFlowLayoutPanel);
             this.Controls.Add(this.settingsButton);
             this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.createPostButton);
             this.Controls.Add(this.searchButton);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.logoPictureBox);
             this.Name = "HomeForm";
             this.Text = "Brick Bulletin - Home";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox logoPictureBox;
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.Button searchButton;
         private System.Windows.Forms.Button createPostButton;
         private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.Button settingsButton;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel feedFlowLayoutPanel;
+        private System.Windows.Forms.FlowLayoutPanel filterFlowLayoutPanel;
+        private System.Windows.Forms.Button loginButton;
     }
 }
 
--- brickBulletinLandingPage/Form1.cs
+++ brickBulletinLandingPage/Form1.cs
@@ -14,24 +14,103 @@ namespace brickBulletinLandingPage
     public partial class HomeForm : Form
     {
         private List<Post> feed = new List<Post>();
+        private List<string> followedUsers = new List<string>();
         public HomeForm()
         {
             InitializeComponent();
             feed.Add(new Post("Thomas Martinez", "TESTING", "THIS IS A TEST POST",new DateTime(12,1,22) ));
+
+            //TEST//
+            Post post = new Post("Rochester Institute of Technology", "Catalytic Converter Thefts", "some ass-hat is still steal catalytic converters" +
+                "around camput and we still haven't done anything about it. ALSO FREEZE FEST IS BACK!!!", new DateTime(12,7,22));
+            GeneratePostControl(post);
+
+            for (int i = 0; i < 20; i++)
+            {
+                GenerateFilterControl(i.ToString());
+            }
+
+            //TEST//
+
+            // Add default RIT filter
+            GenerateFilterControl("RIT");
         }
-    }
-    public struct Post
-    {
-        public Post(string c, string t, string b, DateTime d)
+
+        // Data structure for storing post information
+        public struct Post
         {
-            this.creator = c;
-            this.title = t;
-            this.body = b;
-            this.created = d;
+            public Post(string c, string t, string b, DateTime d)
+            {
+                this.creator = c;
+                this.title = t;
+                this.body = b;
+                this.created = d;
+            }
+            public string creator { get; }
+            public string title { get; }
+            public string body { get; }
+            public DateTime created { get; }
         }
-        public string creator { get; }
-        public string title { get; }
-        public string body { get; }
-        public DateTime created { get; }
+
+        // Create post control and add it to feed
+        public void GeneratePostControl(Post post)
+        {
+            // create groupBox
+            GroupBox postBox = new GroupBox();
+            postBox.Text = post.creator;
+            postBox.Width = 630;
+
+            // title text
+            Label title = new Label();
+            title.Text = post.title;
+            title.Location = new Point(26, 22);
+            postBox.Controls.Add(title);
+
+            // date
+            Label date = new Label();
+            date.Text = post.created.ToString();
+            date.Location = new Point(129, 22);
+            postBox.Controls.Add(date);
+
+            // body text
+            Label body = new Label();
+            body.Text = post.body;
+            body.Location = new Point(65, 62);
+            postBox.Controls.Add(body);
+
+            // add postBos to feed
+            this.feedFlowLayoutPanel.Controls.Add(postBox);
+        }
+
+        // Add verified user filter button
+        public void GenerateFilterControl(string poster)
+        {
+            Button filterButton = new Button();
+            filterButton.Text = poster;
+            filterButton.Size = new Size(70, 30);
+            filterFlowLayoutPanel.Controls.Add(filterButton);
+        }
+
+        // Fill/Refresg feedFlowLayoutBox with up to date posts
+        public void RefrexhFeed(string user)
+        {
+            // Deserialize list of users
+
+            // 
+
+            List<Post> allPosts = new List<Post>();
+            foreach (string fUser in followedUsers)
+            {
+                // Deserialize posts by fUser
+
+                // Add all post by fUser to allPosts
+            }
+
+            // Populate feed with posts from allPosts
+            while (allPosts.Count > 0)
+            {
+
+            }
+        }
     }
 }
