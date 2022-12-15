783796b352ea167d4dae5ab6a218893cb5e3d9f3
thomasismartinez <tjm7126@rit.edu>
2022-12-14 20:06:14 -0500

    a

--- FinalExamJsonSingleton/Program.cs
+++ FinalExamJsonSingleton/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -11,44 +13,72 @@ namespace FinalExamJsonSingleton
     {
         static void Main(string[] args)
         {
+            // create data
+            PlayerSettings player = new PlayerSettings();
+            MySingleton mySingleton = MySingleton.GetInstance();
+            mySingleton.Save(player);
+
+            PlayerSettings loadedSettings = mySingleton.Load();
+            Console.WriteLine(loadedSettings.player_name); // should say "dschuh" if saved and loaded correctly
         }
     }
 
-    public class PlayerStats
+    public class MySingleton
     {
-        private PlayerStats() { }
-        private static PlayerStats stats = new PlayerStats();
-        public static PlayerStats Stats
+        private MySingleton() { }
+
+        private static MySingleton instance = new MySingleton();
+
+        public static MySingleton GetInstance()
         {
-            get { return stats; }
+            return instance;
         }
 
-        // file directory
-        private string dir = "c:/temp/playerStats.json";
-
-        // player stats
-        private string player_name;
-        private int level;
-        private int hp;
-        private List<string> inventory;
-        private string license_key;
-
-        // load stats from file
-        public void Load()
+        public PlayerSettings Load(string path = "c:/temp/player.json")
         {
-
+            StreamReader reader = new StreamReader(path);
+            string data = reader.ReadToEnd();
+            reader.Close();
+            return JsonConvert.DeserializeObject<PlayerSettings>(data);
         }
 
-        // save stats to file
-        public void Save()
+        public void Save(PlayerSettings obj, string path = "c:/temp/player.json")
         {
-
+            string data = JsonConvert.SerializeObject(obj);
+            StreamWriter writer = new StreamWriter(path);
+            writer.Write(data);
+            writer.Close();
         }
+    }
 
-        // print stats to console
-        public void PrintStats()
+    public class PlayerSettings
+    {
+        public string player_name;
+        public int level;
+        public int hp;
+        public string[] inventory;
+        public string liscense_key;
+
+        public PlayerSettings()
         {
-
+            this.player_name = "dschuh";
+            this.level = 4;
+            this.hp = 99;
+            this.inventory = new string[]
+            {
+                "spear",
+                "water bottle",
+                "hammer",
+                "sonic screwdriver",
+                "cannonball",
+                "wood",
+                "Scooby snack",
+                "Hydra",
+                "poisonous potato",
+                "dead bush",
+                "repair powder"
+            };
+            this.liscense_key = "DFGU99-1454";
         }
     }
 }
